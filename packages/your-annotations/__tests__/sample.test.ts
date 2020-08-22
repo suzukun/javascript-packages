ce040c4455d078b6af19ab33a9bdb5de679d7458
suzukun <suzuki-ryota@kayac.com>
2020-08-22 12:58:39 +0900

    chore: remove test file

--- packages/your-annotations/__tests__/sample.test.ts
+++
@@ -1,7 +0,0 @@
-import { yourAnnotations } from '../src/lib/sample';
-
-describe('yourAnnotations', () => {
-    test('yourAnnotations is function', () => {
-        expect(typeof yourAnnotations).toBe('function');
-    });
-});
