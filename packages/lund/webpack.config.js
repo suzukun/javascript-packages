f70e7c0a2079287e797e4e17070378d24d1f9313
suzukun <suzuki-ryota@kayac.com>
2020-11-06 14:24:11 +0900

    chore: lundのwebpack設定。

--- packages/lund/webpack.config.js
+++ packages/lund/webpack.config.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
 const baseConfig = require('../../config/webpack.config.base');
+const browserBaseConfig = require('../../config/webpack.config.browser.base');
 
-module.exports = merge(baseConfig, {});
+module.exports = merge(baseConfig, browserBaseConfig, {});
