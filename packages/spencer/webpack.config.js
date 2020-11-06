a25701b8ef44979c7acac0ac8df1a575ca7a4b1e
suzukun <suzuki-ryota@kayac.com>
2020-11-06 17:34:54 +0900

    chore: raw-loaderを使う。

--- packages/spencer/webpack.config.js
+++ packages/spencer/webpack.config.js
@@ -2,4 +2,14 @@ const { merge } = require('webpack-merge');
 const baseConfig = require('../../config/webpack.config.base');
 const browserBaseConfig = require('../../config/webpack.config.browser.base');
 
-module.exports = merge(baseConfig, browserBaseConfig, {});
+module.exports = merge(baseConfig, browserBaseConfig, {
+    module: {
+        rules: [
+            {
+                test: /\.(vert|frag|glsl)$/,
+                use: 'raw-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+});
